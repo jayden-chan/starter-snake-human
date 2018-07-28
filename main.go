c39ec1704a812d2e9d6f38a7cc13afc5c9390d13
Jayden Chan <jaydencn7@gmail.com>
2018-07-28 00:55:25 -0700

    Initial implementation, basic functions working

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eiannone/keyboard"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/start", Start)
+	http.HandleFunc("/move", Move)
+	http.HandleFunc("/end", End)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9000"
+	}
+
+	// Add filename into logging messages
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	log.Printf("Running server on port %s...\n", port)
+	log.Printf("Keep this terminal focused to recieve input from the keyboard.\nUse the arrow keys to move the snake.")
+	go http.ListenAndServe(":"+port, nil)
+	err := keyboard.Open()
+	if err != nil {
+		panic(err)
+	}
+	defer keyboard.Close()
+
+	fmt.Println("Press ESC to quit")
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			panic(err)
+		} else if key == keyboard.KeyEsc {
+			break
+		}
+		fmt.Printf("You pressed: %q\r\n", char)
+		currentKey = char
+	}
+}
---
+++ routes.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+var currentKey rune = 'w'
+
+type StartRes struct {
+	Color string `json:"color,omitempty"`
+	Taunt string `json:"taunt,omitempty"`
+}
+
+type MoveRes struct {
+	Move string `json:"move"`
+}
+
+// Start is called when a request to /start is received,
+// which will happen once at the beginning of each game.
+// It will contain information about the match including
+// the width and height of the board.
+func Start(res http.ResponseWriter, req *http.Request) {
+	log.Printf("Start req recieved")
+
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(StartRes{
+		Color: "#84D5E2",
+		Taunt: "NOT A HUMAN...",
+	})
+
+	res.Write([]byte("\n"))
+}
+
+// Move is called on each game tick. We must send our
+// move response here.
+func Move(res http.ResponseWriter, req *http.Request) {
+	move := MoveRes{}
+	switch currentKey {
+	case 'w':
+		move = MoveRes{Move: "up"}
+	case 'a':
+		move = MoveRes{Move: "left"}
+	case 's':
+		move = MoveRes{Move: "down"}
+	case 'd':
+		move = MoveRes{Move: "right"}
+	}
+	HTTPRes(res, move)
+}
+
+// End is only called at the end of the game and is
+// essentially useless I think..
+func End(res http.ResponseWriter, req *http.Request) {
+	log.Printf("End req recieved")
+}
+
+// HTTPRes sends an HTTP response to the given ResponseWriter
+func HTTPRes(res http.ResponseWriter, obj interface{}) {
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(obj)
+	res.Write([]byte("\n"))
+}
