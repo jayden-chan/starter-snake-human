ee1e7120cb76cdf52cc06c131290f123513e4686
Jayden Chan <jaydencn7@gmail.com>
2018-07-28 15:03:59 -0700

    Update readme

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # starter-snake-human
 Takes input from the standard in to allow you to train your snakes
+
+## Installation
+Select the appropriate executable from the `/bin` directory and run it from your terminal.
+
+### Windows
+From the directory containing the executable:
+`> starter-snake-human.exe`
+
+### Mac OS / Linux
+From the directory containing the executable:
+`$ ./starter-snake-human`
+
+## Usage
+Use the W A S D keys to move the snake around. Pres ESC to exit.
+
+### Arguments
+To change the color of the snake:
+`--color "#ffffff"`
+
+To change the taunt:
+`--taunt "i am snak"`
+
+To change the port:
+`--port "8080"`
+
+For example, the following will run a pink snake on port 8080 with taunt "i am snak"
+`./starter-snake-human --color "#ff00ff" --port "8080" --taunt "i am snak"`
--- main.go
+++ main.go
@@ -21,28 +21,35 @@ func main() {
 	http.HandleFunc("/move", Move)
 	http.HandleFunc("/end", End)
 
+	port := "9000"
+
+	// Parse --color command line argument
 	for i, _ := range os.Args {
 		if i == 0 {
 			continue
 		}
-		if os.Args[i] == "--color" && len(os.Args) > i+1 {
+		switch {
+		case os.Args[i] == "--color" && len(os.Args) > i+1:
 			snakeColor = os.Args[i+1]
 			log.Println("Snake color set to", snakeColor)
+		case os.Args[i] == "--port" && len(os.Args) > i+1:
+			port = os.Args[i+1]
+			log.Println("Port set to", port)
+		case os.Args[i] == "--taunt" && len(os.Args) > i+1:
+			snakeTaunt = os.Args[i+1]
+			log.Println("Taunt set to", snakeTaunt)
 		}
-	}
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "9000"
 	}
 
 	// Add filename into logging messages
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	log.Printf("Running server on port %s...\n", port)
-	log.Printf("Keep this terminal focused to recieve input from the keyboard.\nUse the arrow keys to move the snake.")
+	log.Printf("Server running on port %s...\n", port)
+	fmt.Printf("\nKeep this terminal focused to recieve input from the keyboard.\nUse the W A S D keys to move the snake.\n")
 
+	// Run the HTTP server in a new goroutine
 	go http.ListenAndServe(":"+port, nil)
 
+	// Take input from the keyboard
 	err := keyboard.Open()
 	if err != nil {
 		panic(err)
@@ -57,6 +64,8 @@ func main() {
 		} else if key == keyboard.KeyEsc {
 			break
 		}
+		// If the provided move will result in the snake going back
+		// on itself, don't add it to the queue
 		switch {
 		case char == 'w' && lastKey == 's':
 			break
